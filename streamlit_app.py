c6333e99d0a887ac536f55c7db27b1e72933d390
Tiago Holanda <tfholanda@gmail.com>
2024-10-23 18:55:20 -0300

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,8 +180,7 @@ st.set_page_config(
     initial_sidebar_state='expanded',
     menu_items={
         'Get Help': 'https://www.example.com/help',
-        'Report a bug': 'https://www.example
-.com/bug',
+        'Report a bug': 'https://www.example.com/bug',
         'About': 'Dashboard FITec v1.0'
     }
 )
